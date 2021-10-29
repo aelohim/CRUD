4f03445dcf6e1ea4f61177b8ecdfb0245074995b
Aelohim <aelohim2@gmail.com>
2021-10-29 11:05:48 -0300

    dashboard en proceso

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { AgregarEmpleadoComponent } from './components/agregar-empleado/agregar-empleado.component';
 import { EditarEmpleadoComponent } from './components/editar-empleado/editar-empleado.component';
 import { ListarEmpleadoComponent } from './components/listar-empleado/listar-empleado.component';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardModule } from './components/dashboard/dashboard.module';
 
 const routes: Routes = [
-  {path: '', pathMatch:'full', redirectTo: 'agregar-empleado'},
+  {path: '', pathMatch:'full', redirectTo: 'login'},
+  {path: 'login', component: LoginComponent },
+  {path: 'dashboard', loadChildren: () => import('./components/dashboard/dashboard.module').then(x => x.DashboardModule)},
   {path: 'agregar-empleado', component:AgregarEmpleadoComponent},
   {path: 'editar-empleado/:id', component:EditarEmpleadoComponent},
   {path: 'listar-empleado', component:ListarEmpleadoComponent}
